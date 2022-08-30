720ea8de6e868bd14d7266712ddb53f1f9025210
unknown <fluckiger.jonas.dev@gmail.com>
2022-08-30 11:48:22 +0200

    initial commit

---
+++ client/client.py
@@ -0,0 +1,39 @@
+import requests
+import time
+
+earth = {'name': 'Earth', 'diameter' : 123123}
+jupiter = {'name': 'Jupiter', 'diameter' : 34564}
+saturn = {'name': 'Saturn', 'diameter' : 234234}
+mars = {'name': 'Mars', 'diameter' : 645634}
+venus = {'name': 'Venus', 'diameter' : 234234}
+
+
+tmpTime = time.time()
+x = requests.post('http://front-end:80/planets', params=earth)
+x = requests.post('http://front-end:80/planets', params=jupiter)
+x = requests.post('http://front-end:80/planets', params=mars)
+x = requests.post('http://front-end:80/planets', params=saturn)
+x = requests.post('http://front-end:80/planets', params=venus)
+print(str(time.time() - tmpTime) + "s for post requests")
+
+while True:
+    
+    tmpTime = time.time()
+    for i in range(0, 100):
+        requests.get('http://front-end:80/planets?name=Earth')
+    print(str(time.time() - tmpTime) + "s for get requests")
+
+
+    tmpTime = time.time()
+    for i in range(0, 100):
+        requests.get('http://front-end:80/pi')
+    print(str(time.time() - tmpTime) + "s for PI computation request")
+
+    tmpTime = time.time()
+    for i in range(0, 100):
+        x = requests.get('http://front-end:80/home')
+    print(str(time.time() - tmpTime) + "s for home page request")
+    print(x.content)
+
+
+    time.sleep(1)
---
+++ server/app.js
@@ -0,0 +1,77 @@
+var express = require('express')
+var app = express()
+app.listen(3000)
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://back-end:27017/";
+var diameter = "500";
+
+
+app.get('/home', function (req, res) {
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="fr-CH">
+        <head>
+            <meta charset="UTF-8">
+            <title>Planets API</title>
+        </head>
+        <body>
+            <h1>Planets API</h1>
+            <p>Bienvenue sur l'API des plan&egrave;tes!</p>
+        </body>
+    </html>
+    `);
+});
+
+
+app.get('/pi', function (req, res) {
+    let i = 1n;
+    let x = 3n * (10n ** 10000n);
+    let pi = x;
+    while (x > 0) {
+        x = x * i / ((i + 1n) * 4n);
+        pi += x / (i + 2n);
+        i += 2n;
+    }
+    result = (pi / (10n ** 20n)).toString();
+    res.send(result)
+});
+
+
+app.get('/planets', function (req, res) {
+    let name_val = req.query.name;
+    try {
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            var dbo = db.db("space");
+            dbo.collection("planets").findOne({ name: name_val }, function (err, result) {
+                if (err) { throw err; }
+                else if (result===null) { throw "null result"}
+                
+                db.close();
+                res.send(result.diameter);
+            });
+        });
+    } catch (err) {
+        res.sendStatus(500);
+    }
+});
+
+app.post('/planets', function (req, res) {
+    let name_val = req.query.name;
+    let diameter_val = req.query.diameter;
+    try {
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+            var dbo = db.db("space");
+            var myobj = { name: name_val, diameter: diameter_val };
+            dbo.collection("planets").insertOne(myobj, function (err, res) {
+                if (err) throw err;
+                db.close();
+            });
+        });
+    } catch (err) {
+        res.sendStatus(500);
+    }
+    res.send("Ok!")
+});
