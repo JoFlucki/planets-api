79edba74c6a8d4e0479f3ff3eab375a74258338e
unknown <fluckiger.jonas.dev@gmail.com>
2022-08-31 15:26:32 +0200

    ReplicaSets MongoDB not working yet, WIP

---
+++ server/request.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://mongo-0.mongo,mongo-1.mongo,mongo-2.mongo";
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("space");
+    var myobj = { name: "name_val", diameter: 2342433 };
+    dbo.collection("planets").insertOne(myobj, function (err, res) {
+        if (err) throw err;
+        console.log("ok!");
+        db.close();
+    });
+});
